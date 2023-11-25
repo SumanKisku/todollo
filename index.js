dceebf1386450aac7a94ede588882ae82d5a3704
Suman Kisku <sumankisku1@gmail.com>
2023-11-26 02:22:15 +0530

    Added express session, mongo store for keeping session

--- index.js
+++ index.js
@@ -3,20 +3,37 @@ const cli = require("cli-color");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRouter = require("./routes/User")
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const PORT = process.env.PORT || 8001;
 
 app.use(express.json());
-app.use(express.urlencoded( { extended: true } ));
+app.use(express.urlencoded( { extended: false } ));
 
-app.use("/api/user", userRouter);
 
 mongoose.connect("mongodb://127.0.0.1:27017/todollo").then(() => {
     console.log(cli.green.bold(`MongoDB connected`));
 }).catch((err) => console.log(cli.red.bold("Mongo error", err)));
 
+app.use(session({
+    name: "session.id",
+    secret: "hello world",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/todollo" })
+}));
+
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
     console.log(cli.green.bold(`The server is running on ${PORT}`));
 })
+
+// TODO
+// User schema - Done
+// Authentication -
+// Board schema
+// Lists schema
+// Cards schema
